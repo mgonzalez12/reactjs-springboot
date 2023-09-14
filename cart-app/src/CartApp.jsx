b88b4d029aaba6958d2b395d9c6920b2ae91443a
mgonzalez12 <ing.marcosgonzalez1@gmail.com>
2023-09-14 11:03:25 +0200

    feat(cart-app) commit sessionStorage
    implement SessionStorage

--- cart-app/src/CartApp.jsx
+++ cart-app/src/CartApp.jsx
@@ -3,12 +3,7 @@ import { CartView } from "./components/CartView"
 import { CatalogView } from "./components/CatalogView"
 import { products } from "./data/products"
 
-const initialCartItems = [
-    /* {
-         product: {},
-         quantity: 0,
-     }*/
-]
+const initialCartItems = JSON.parse(sessionStorage.getItem('cart')) || [];
 
 export const CartApp = () => {
 
